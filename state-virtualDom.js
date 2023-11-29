fd92621f5e1a68f8979629f62751b468bd157b22
승희 <bangseunghee1108@gmail.com>
2023-11-29 23:05:30 +0900

    주석 작성

--- state-virtualDom.js
+++ state-virtualDom.js
@@ -2,6 +2,10 @@ export function load() {
   document.addEventListener('DOMContentLoaded', function () {
     
     function createElement(type, props, ...children) {
+      // type : Element의 타입을 나타내는 문자열 또는 React Component 이다.
+      // props : Element의 속성을 나타내는 객체로 생략 가능하다.
+      // children : Element의 자식 요소를 나타내는 인자로 생략 가능, 여러 개의 인자를 받을 수 있다.
+      // ... 은 나머지 매개변수로서 해당 함수에 전달된 모든 나머지 인자들을 배열로 수집한다.
       return { type, props, children }
     }
     
