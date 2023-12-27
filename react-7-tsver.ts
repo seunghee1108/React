28ea34dcfb6fe420775408b2a9982099027231a9
승희 <bangseunghee1108@gmail.com>
2023-12-27 22:18:58 +0900

    classroom-React-7 예제 typescript-version

---
+++ react-7-tsver.ts
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const PokemonList: React.FC = () => {
+  
+  const pokemon: string[] = ["피카츄", "라이츄", "파이리", "꼬부기"];
+
+  const updatePokemon: string[] = pokemon.map((pokemonName) => {
+    if (pokemonName === "파이리") {
+      return "버터풀";
+    }
+    return pokemonName;
+  });
+  
+  const pokemonElements = updatePokemon.map((ㅎokemonName, index) => (
+    <div key={index}>
+      <p>{pokemonName}</p>
+    </div>
+  ));
+  
+  return <div>{pokemonElements</div>;
+
+};
+
+export default PokemonList;
