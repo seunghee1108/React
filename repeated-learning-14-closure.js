5b04422f46abcd40fd4f4db6a96ae1cf513ee72c
승희 <bangseunghee1108@gmail.com>
2023-12-28 22:11:43 +0900

    classroom-Repeated-learning-14 callback closure 주석 작성

--- repeated-learning-14-closure.js
+++ repeated-learning-14-closure.js
@@ -1,10 +1,11 @@
 const stringExample = "참깨빵 위에 순쉬고기 패티 두 장 특별한 소스 양상추 치즈 피클 양파까지";
 
 function createStringAppender() {
-  let body = "";
+  let body = "";  // 클로저에 의해 "기억"될 변수
+  // 리턴이 함수로 처리되어, body를 업데이트 할 수 있으며, React에서 상당히 많이 사용되는 패턴입니다.
   return function(stringArray, callback) {
     stringArray.forEach((elemnet) => {
-      body += elemnet;
+      body += elemnet;  // 클로저를 사용하여 body 변수를 업데이트
     });
     callback(body);
   };
@@ -16,6 +17,7 @@ function splitString(stringParms) {
   return stringParms.split(' ');
 }
 
+// appendString를 사용
 appendString(splitString(stringExample), (body) => {
   console.log(body);
   console.log(body.length);
